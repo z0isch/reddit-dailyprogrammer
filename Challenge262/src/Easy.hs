aba4e1a1abb88bd9bb566d74a923e393d34ad1ba
unknown <aruf@itec-aruf.bellarmine.edu>
2016-04-11 17:11:58 -0400

    Bonus #1 and #2

--- Challenge262/src/Easy.hs
+++ Challenge262/src/Easy.hs
@@ -1,25 +1,38 @@
 module Easy where
 
+import           Control.Monad
 import           Data.List
 import           Data.Maybe
 import           Text.Trifecta
 import           Text.Trifecta.Delta
 
-data EasyParsed = EasyString String | EasyNumber Double
+data EasyParsed = EasyArray [Double] | EasyString String
   deriving (Show)
 
 test1 = "123"
 test2 = "44.234"
 test3 = "0x123N"
+test4 = "3.23e5"
+test5 = "1293712938712938172938172391287319237192837329"
+test6 = ".25"
+test7 = "123 234 345"
+test8 = "2015 4 4`Challenge #`261`Easy"
+test9 = "234.2`234ggf 45`00`number string number (0)"
 
 parseTestData t = parseString easyParse (Columns (genericLength t) 0) t
 
-easyParse = choice [try numberParser, stringParser]
+mParseTestData t = parseString mEasyParse (Columns (genericLength t) 0) t
 
-stringParser = EasyString <$> some anyChar
+mEasyParse = easyParse `sepBy1` char '`'
 
-numberParser = p <$> some digit <*> choice [end, decimalPart]
+easyParse = choice [try arrayParser, stringParser]
+
+arrayParser = EasyArray <$> numberParser `sepBy1` char ' '
+
+stringParser = EasyString <$> some (notChar '`')
+
+numberParser = p <$> option "0" (some digit) <*> optional ((++) <$> numPart '.' <*> option "" (numPart 'e')) <* notFollowedBy (noneOf [' ','`'])
   where
-    p x y = EasyNumber $ read (x ++ y)
-    end = const ".0" <$> eof
-    decimalPart = (:) <$> char '.' <*> some digit
+    p x (Just y) = read (x ++ y)
+    p x Nothing = read x
+    numPart c = (:) <$> char c <*> some digit
--- Challenge262/src/Main.hs
+++ Challenge262/src/Main.hs
@@ -7,8 +7,8 @@ import           Text.Trifecta
 isString x (E.EasyString y) = x == y
 isString _ _ = False
 
-isNumber x (E.EasyNumber y) = x == y
-isNumber _ _ = False
+isArray x (E.EasyArray y) = x == y
+isArray _ _ = False
 
 correctParse (Success True) = True
 correctParse _ = False
@@ -16,6 +16,12 @@ correctParse _ = False
 main :: IO ()
 main = hspec $ do
   describe "Easy" $ do
-    it "Test 1" $ correctParse (isNumber 123 <$> E.parseTestData E.test1) `shouldBe` True
-    it "Test 2" $ correctParse (isNumber 44.234 <$> E.parseTestData E.test2) `shouldBe` True
+    it "Test 1" $ correctParse (isArray [123] <$> E.parseTestData E.test1) `shouldBe` True
+    it "Test 2" $ correctParse (isArray [44.234] <$> E.parseTestData E.test2) `shouldBe` True
     it "Test 3" $ correctParse (isString "0x123N" <$> E.parseTestData E.test3) `shouldBe` True
+    it "Test 4" $ correctParse (isArray [3.23e5] <$> E.parseTestData E.test4) `shouldBe` True
+    it "Test 5" $ correctParse (isArray [1293712938712938172938172391287319237192837329] <$> E.parseTestData E.test5) `shouldBe` True
+    it "Test 6" $ correctParse (isArray [0.25] <$> E.parseTestData E.test6) `shouldBe` True
+    it "Test 7" $ correctParse (isArray [123,234,345] <$> E.parseTestData E.test7) `shouldBe` True
+    it "Test 8" $ correctParse (and . zipWith ($) [isArray [2015,4,4],isString "Challenge #",isArray [261],isString "Easy"] <$> E.mParseTestData E.test8) `shouldBe` True
+    it "Test 9" $ correctParse (and . zipWith ($) [isArray [234.2],isString "234ggf 45",isArray [0.0],isString "number string number (0)"] <$> E.mParseTestData E.test9) `shouldBe` True
